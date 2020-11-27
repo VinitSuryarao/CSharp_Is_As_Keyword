8a9dd0b74009ca018c82d0d10813ef11dcd2957a
Vinit Suryarao <suryarao.vinit@hotmail.com>
2020-11-27 20:04:29 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsAsKeyword
+{   
+        class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // As Keyword convert one to another
+            // It converts only reference type or nullable 
+            object Value1 = 123;
+            string Data1 = Value1 as string;
+
+            // Is Keyword check conversion is compatible or not
+            // It return true/false
+            int Value3 = 874;
+            Boolean Result = Value3 is string;
+        }
+    }
+}
